0e86019adc25af107851beb71fdad3d790e62440
achalkatkar <11achalkatkar@gmail.com>
2023-09-23 23:35:26 +0530

    added java first program

---
+++ first.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class first 
+{
+    public static void main(String args[])
+    {
+        int a;
+        System.out.println("enter number");
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        System.out.println(a);
+    }
+}
