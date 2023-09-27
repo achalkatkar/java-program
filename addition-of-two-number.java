2cf6034b9a956ba624bed67c272180d2ee937837
achalkatkar <11achalkatkar@gmail.com>
2023-09-27 23:43:03 +0530

    added additon of two number

--- addition-of-two-number.java
+++ addition-of-two-number.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class addition{
 
     public static void main(String [] args){
-        
+        int x, y, sum;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        x = myObj.nextInt();
+
+        System.out.println("Enter the second number");
+        y = myObj.nextInt();
+
+        sum = x + y;
+        System.out.println("Addition of two number :"+sum);
+
+
+
     }
     
 }
