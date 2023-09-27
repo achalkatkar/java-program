070fadd74aaf78c9fef21e07d83276e5491dd829
achalkatkar <11achalkatkar@gmail.com>
2023-09-27 23:47:47 +0530

    added comment

--- addition-of-two-number.java
+++ addition-of-two-number.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
+import java.util.Scanner;   // import the scanner class
 
-public class addition{
+public class addition {
 
     public static void main(String [] args){
         int x, y, sum;
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter the first number");
-        x = myObj.nextInt();
+        Scanner myObj = new Scanner(System.in);    // create a scanner onject
+        System.out.println("Enter the first number"); 
+        x = myObj.nextInt();               // read user input
 
         System.out.println("Enter the second number");
         y = myObj.nextInt();
 
-        sum = x + y;
-        System.out.println("Addition of two number :"+sum);
+        sum = x + y;         // calculate the sum of x + y 
+        System.out.println("Addition of two number :"+sum);   // print the sum 
 
 
 
